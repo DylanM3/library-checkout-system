5da3d6963bc9c30f46e1a39f016a5ea2f5360cdf
Dylan Moore <dylanxmoore0@gmail.com>
2026-09-19 15:56:51 +0000

    FEAT: Updated login to store successfully loged in users information

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 # IMPORTS AND INITS
 import json
-current_user = []
 
 # LOAD DATABASE.JSON INTO PYTHON
 with open("database.json") as db:
@@ -15,36 +14,42 @@ books_dict = data.pop("books", {})
 members_dict = data.pop("members", {})
 loans_dict = data.pop("loans", {})
 
-def login()
+# SIGN IN FUNCTION
+# Loops over every member with O(n) complexity until ID matched or it returns failure
+def login(member_id, password):
 
+    # Initialise variable
+    member_details = None
 
+    for member in members_dict:
 
+        # Grab the correct login info from the dictionarys
+        system_member_id = member["member_id"]
+        system_password = member["password"]
 
+        # Compare the information to the correct information
+        if member_id == system_member_id:
+            member_id_approved = True
+        else:
+            member_id_approved = False
 
+        if password == system_password:
+            password_approved = True
+        else:
+            password_approved = False
 
+        # Check if ALL the information is correct
+        if member_id_approved and password_approved:
+            member_details = member
 
+    # Check if a member was found
+    if member_details:
+        print(f"Welcome, {member_details["name"]}.")
+    else:
+        print("Login Failed.\nPlease re-run the program and try again.")
 
+        
+member_id = input("Member ID: ")
+password = input("Password: ")
 
-
-
-
-
-
-# # SIGN IN FUNCTION
-# # Loops over every member with O(n) complexity until ID matched or it returns failure
-# def login(given_member_id):
-#     for member in members_dict:
-#         current_member_id = member["member_id"]
-#         if given_member_id == current_member_id:
-#             return True
-#     return False # Checked all members, no ID matched
-
-# given_member_id = input("Member ID Number: ")
-# login_results = login(given_member_id)
-
-# if login_results == True:
-#     index_number = int(members_dict[1:]) - 1
-#     current_user.append(members_dict[index_number])
-#     print(current_user)
-# else:
-#     print("ID not recognised.\nPlease try again.")
+member_details = login(member_id, password)
